9e0e7e793968717da22dfabe035032c46c496235
Amit Khare <95758619+amkyash@users.noreply.github.com>
2022-07-26 10:13:43 +0530

    Added Comments

--- DP/PascalsTriangle.py
+++ DP/PascalsTriangle.py
@@ -1,8 +1,13 @@
 def pas(n):
+    #First Row
     res=[[1]]
+    
+    #for the rest of rows
     for i in range(n-1):
+        #creating temp row  for adding Extra 0 at start and end
         temp=[0]+ res[-1]+[0]
         row=[]
+
         for j in range( len(res[-1]) + 1 ):
             row.append(temp[j]+temp[j+1])
         res.append(row)
